de792b033e0dd56054796caa8e0279d795de5c80
ombayus <omerbayramcavus@gmail.com>
2023-08-16 21:04:13 +0300

    feat: add status options

--- src/pages/MyBookList/components/Filter.js
+++ src/pages/MyBookList/components/Filter.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Dropdown } from "primereact/dropdown";
 
 import { InputText } from "primereact/inputtext";
+import statusOptions from "../constants/status.options";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
@@ -10,10 +11,9 @@ export default () => {
     <div className="flex align-items-center justify-content-between py-3 px-5 flex-wrap">
       <div className="text-500 w-6 md:w-2 font-medium">
         <Dropdown
-          optionLabel="name"
-          optionValue="id"
-          options={[]}
-          placeholder="Select a booklist"
+          value={statusOptions[0].value}
+          options={statusOptions}
+          placeholder="Select a Status"
         />
       </div>
       <div className="w-6 md:w-2 flex justify-content-end">
